ff2fe67aec81ddb3816a038f0cef4b9b4e4e12d1
root <root@localhost.localdomain>
2018-08-31 09:17:58 +0800

    add dfwport

---
+++ dfwport.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+firewall-cmd --add-port=2376/tcp --permanent
+firewall-cmd --add-port=2377/tcp --permanent
+firewall-cmd --add-port=7946/tcp --permanent
+firewall-cmd --add-port=7946/udp --permanent
+firewall-cmd --add-port=4789/udp --permanent
+firewall-cmd --reload
+
+systemctl restart docker
+
