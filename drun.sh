814a83926e0737ddd0897071cb8e0fe8717a4801
ymwang78 <wangym@gmail.com>
2018-09-03 16:48:25 +0800

    Update drun.sh

--- drun.sh
+++ drun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --name $1 --privileged -ti -e container=docker  -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /opt/docker/$1:/zdata -p $3 -p $4 --net doverlay $2 /usr/sbin/init
+sudo docker run --name $1 --privileged -ti -e container=docker  -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /opt/docker/$1:/zdata -p $3 -p $4 -p $5 --net ffoverlay $2 /usr/sbin/init
