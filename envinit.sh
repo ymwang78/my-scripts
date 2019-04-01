50a1f3d577637091b7c872f2eb877977cf0892f1
root <root@localhost.localdomain>
2019-04-01 09:15:51 +0800

    pgsql -> 11

--- envinit.sh
+++ envinit.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+rpm -ivh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+rpm -ivh https://download.postgresql.org/pub/repos/yum/11/redhat/rhel-7-x86_64/pgdg-centos11-11-2.noarch.rpm
+
+yum install postgresql11-libs boost hiredis gperftools-libs mariadb-libs
+
 yum install telnet vim less gdb net-tools git lrzsz wget;
 
