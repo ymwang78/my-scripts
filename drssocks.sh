d117b65a4ec5236003b8869ed6f14d95c65b24be
wangym <wangym@gmail.com>
2023-08-19 12:47:51 +0800

    fix drssocks

--- drssocks.sh
+++ drssocks.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -d -p $2:1984 --net ffoverlay --name $1 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 1984 -k $3 -m aes-256-cfb
+#ssocker outport username password
+sudo docker run -d -p $1:1984 --name $2 oddrationale/docker-shadowsocks -s 0.0.0.0 -p 1984 -k $3 -m aes-256-cfb
