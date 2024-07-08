0a56080fbdc28ed24d06568d438372e09280606e
ymwang78 <wangym@gmail.com>
2024-07-08 17:57:06 +0800

    Update debian_pypip.sh

--- debian_pypip.sh
+++ debian_pypip.sh
@@ -1,4 +1,4 @@
 #create venv first
 sudo apt install uwsgi uwsgi-plugin-python3
 pip install --upgrade pip
-pip install  django django-qr-code xlwt django_redis Pillow markdown psycopg2-binary flask flask_sqlalchemy flask_redis requests apscheduler cryptography pycryptodome  qrcode  PySocks 
+pip install  django django-qr-code xlwt django_redis Pillow markdown psycopg2-binary flask flask_sqlalchemy flask_redis requests apscheduler cryptography pycryptodome qrcode PySocks phonenumbers
