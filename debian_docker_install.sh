c215f8db2d4be8868f4c0e22d9fce818a7a69822
wangym <wangym@Debian13Aarch64.debian13aarch64.100833.com>
2025-10-02 08:49:05 -0400

    add current user to docker

--- debian_docker_install.sh
+++ debian_docker_install.sh
@@ -10,3 +10,5 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #sudo docker run hello-world
+sudo usermod -aG docker $USER
+
